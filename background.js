a2207c069dad1105f69c7075bbf148434cd15fab
Kaleb Raymond <43358126+KalebRaymond@users.noreply.github.com>
2018-11-04 19:25:34 -0500

    Add files via upload

---
+++ background.js
@@ -0,0 +1,47 @@
+/*
+Called when the item has been created, or when creation failed due to an error.
+We'll just log success/failure here.
+*/
+function onCreated() {
+  if (browser.runtime.lastError) {
+    console.log(`Error: ${browser.runtime.lastError}`);
+  } else {
+    console.log("Item created successfully");
+  }
+}
+
+/*
+Called when the item has been removed.
+We'll just log success here.
+*/
+function onRemoved() {
+  console.log("Item removed successfully");
+}
+
+/*
+Called when there was an error.
+We'll just log the error here.
+*/
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+/*
+Create all the context menu items.
+*/
+browser.menus.create({
+  id: "log-selection",
+  title: browser.i18n.getMessage("menuItemSelectionLogger"),
+  contexts: ["selection"]
+}, onCreated);
+
+The click event listener, where we perform the appropriate action given the
+ID of the menu item that was clicked.
+*/
+browser.menus.onClicked.addListener((info, tab) => {
+  switch (info.menuItemId) {
+    case "log-selection":
+      console.log(info.selectionText);
+      break;
+  }
+});
